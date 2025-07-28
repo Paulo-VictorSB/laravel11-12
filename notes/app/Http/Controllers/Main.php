13ee206de9c124edaf8670c39e316bc60ebdb65e
Paulo-VictorSB <paulovdbarbosa@gmail.com>
2025-07-28 11:57:13 -0300

    aula025 - Relacionamento entre tabelas com Eloquent ORM: BelongsTo, HasMany e afins.

--- notes/app/Http/Controllers/Main.php
+++ notes/app/Http/Controllers/Main.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Main extends Controller
@@ -9,6 +10,15 @@ class Main extends Controller
     public function index()
     {
         // load users notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        echo '<pre>';
+        print_r($user);
+        print_r($notes);
+
+        die();
 
         // show home view
         return view('Home');
