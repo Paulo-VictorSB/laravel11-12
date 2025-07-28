7c8540de55d1b7e8a84c8b3dcbf91d03d72d7cb2
Paulo-VictorSB <paulovdbarbosa@gmail.com>
2025-07-28 10:03:28 -0300

    aula020 - Middleware: Introdução ao mesmo, auxilio e nova funcionalidade de routes ->group

--- notes/app/Http/Controllers/Main.php
+++ notes/app/Http/Controllers/Main.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class Main extends Controller
 {
-    
+    public function index()
+    {
+        echo 'Im inside the app!';
+    }
+
+    public function newNotes()
+    {
+        echo 'I am creating a new note!';
+    }
 }
--- notes/routes/web.php
+++ notes/routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Http\Controllers\Auth;
+use App\Http\Controllers\Main;
+use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [Auth::class, 'login']);
 Route::post('/loginSubmit', [Auth::class, 'loginSubmit']);
-Route::get('/logout', [Auth::class, 'logout']);
+
+Route::middleware([CheckIsLogged::class])->group(function () {
+    Route::get('/', [Main::class, 'index']);
+    Route::get('/newNote', [Main::class, 'newNote']);
+    Route::get('/logout', [Auth::class, 'logout']);
+});
