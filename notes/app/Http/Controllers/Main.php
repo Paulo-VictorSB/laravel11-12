98a514b192378f3eee6ab7beda1742cf1ad4dcf1
Paulo-VictorSB <paulovdbarbosa@gmail.com>
2025-07-30 11:48:01 -0300

    aula029 - Aprendendo a criar um Service para utilização em escopo global na apliacação dos métodos.

--- notes/app/Http/Controllers/Main.php
+++ notes/app/Http/Controllers/Main.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -26,25 +27,15 @@ class Main extends Controller
 
     public function editNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "im editing note with id = $id";
     }
 
     public function deleteNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "im deleting note with id = $id";
     }
 
-    private function decryptId($id)
-    {
-        // check if $id is crypted
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect('/');
-        }
 
-        return $id;
-    }
 }
---
+++ notes/app/Services/Operations.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
+
+class Operations
+{
+    public static function decryptId($value)
+    {
+        // check if $id is crypted
+        try {
+            $value = Crypt::decrypt($value);
+        } catch (DecryptException $e) {
+            return redirect('/');
+        }
+
+        return $value;
+    }
+}
