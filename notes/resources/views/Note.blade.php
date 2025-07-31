69dd53aae0f656f1b24d838a9f8847a7dccf10be
Paulo-VictorSB <paulovdbarbosa@gmail.com>
2025-07-31 11:00:46 -0300

    aula033 - Alterando os dados das notas e apresentando na view home os dados do updated at casos a nota tenha sido alterada

--- notes/resources/views/Note.blade.php
+++ notes/resources/views/Note.blade.php
@@ -5,7 +5,10 @@
                 <div class="col">
                     <h4 class="text-info">{{ $note['title'] }}</h4>
                     <small class="text-secondary"><span class="opacity-75 me-2">Created
-                            at:</span>{{ date('d-m-Y H:i:s', strtotime($note['created_at'])) }}</small>
+                            at:</span><strong>{{ date('d-m-Y H:i:s', strtotime($note['created_at'])) }}</strong></small>
+                    @if($note['updated_at'] != $note['created_at'])
+                        <small class="text-secondary ms-5"><span class="opacity-75 me-2">Last update:</span><strong>{{ date('d-m-Y H:i:s', strtotime($note['updated_at'])) }}</strong></small>
+                    @endif
                 </div>
                 <div class="col text-end">
                     <a href="{{ Route('edit', ['id' => Crypt::encrypt($note['id'])]) }}" class="btn btn-outline-secondary btn-sm mx-1"><i
