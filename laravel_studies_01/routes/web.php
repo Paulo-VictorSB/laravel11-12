52c1b283c386095d773695e3927ec8a796aa57f3
Paulo-VictorSB <paulovdbarbosa@gmail.com>
2025-08-05 09:39:02 -0300

    aula 001 - Routes: como criar, tipos de rotas, padrões, etc.

--- laravel_studies_01/routes/web.php
+++ laravel_studies_01/routes/web.php
@@ -1,5 +1,32 @@
 <?php
 
+use App\Http\Controllers\Main;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+// Assinatura base de uma rota
+// Route::verb('uri', callback);
+
+// Rota com função anonima
+Route::get('/rota', function() {
+    return '<h1>Olá Laravel!</h1>';
+});
+
+Route::get('/injection', function(Request $request) {
+    return dd($request);
+});
+
+Route::match(['get', 'post'], '/getOrPost', function() {
+    return "<h1>Aceita GET ou POST</h1>";
+});
+
+Route::any('/any', function() {
+    return "<h1>Aceita qualquer HTTP VERB</h1>";
+});
+
+Route::get('/index', [Main::class, 'index']);
+Route::get('/about', [Main::class, 'about']);
+
+Route::redirect('/home', 'index');
+
+Route::view('/iindex', 'home');
