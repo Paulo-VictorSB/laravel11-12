bc8c31af25ae556c9722a2b32dad17205ffd2c7f
Paulo Victor <paulovdbarbosa@gmail.com>
2025-08-12 20:20:03 -0300

    aula006 - Single Action Controller && Resource Controller

--- laravel_studies_01/routes/web.php
+++ laravel_studies_01/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Main;
+use App\Http\Controllers\SAC;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [Main::class, 'initMethod'])->name('init');
 Route::get('/view', [Main::class, 'viewPage'])->name('view');
+
+// route to csa
+Route::get('/single', SAC::class);
