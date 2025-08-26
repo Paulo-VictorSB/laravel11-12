4fbe235893c9d9923cfdeb2ea4deeeb8403c58aa
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-08-26 14:25:01 -0300

    Importante recursos no layout

--- laravel_studies_03/resources/views/home.blade.php
+++ laravel_studies_03/resources/views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.main_layout')
 
-@section('page_title', 'Home Page')
+@section('page_title', 'Homepage')
 
 @section('content')
-    <p>Conteúdo da minha view</p>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col card p-5 bg-secondary text-center">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque blanditiis voluptas pariatur eaque sapiente. Eveniet tempora, vitae qui quisquam doloribus debitis voluptates voluptate dolore eum excepturi sapiente quibusdam id! Quo.
+            </div>
+        </div>
+    </div>
 @endsection
--- laravel_studies_03/resources/views/layouts/main_layout.blade.php
+++ laravel_studies_03/resources/views/layouts/main_layout.blade.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="{{ asset('/assets/bootstrap/bootstrap.min.css') }}">
     <title>@yield('page_title')</title>
 </head>
-
 <body>
 
-    <h1>Topo do Layout</h1>
+    @include('layouts.navbar')
 
     @yield('content')
 
-    <h1>Fundo do Layout</h1>
-
+    {{-- Bootstrap --}}
+    <script src="{{ asset('/assets/bootstrap/bootstrap.bundle.css') }}"></script>
 </body>
-
 </html>
