05e84e3fa33e20a7cc5972023eb19681391a599e
Paulo-VictorSB <paulo.vdbarbosa@gmail.com>
2025-08-28 08:31:28 -0300

    Blade layouts com components

--- countries_and_capitals/routes/web.php
+++ countries_and_capitals/routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'Olá mundo!';
-});
-
-Route::get('/show_data', [MainController::class, 'showData']);
+Route::view('/', 'home');
